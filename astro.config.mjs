960e593e96813afe2b4585f29af95e15698e16d1
Aditya Uke <aditya.u@systenics.com>
2025-12-18 16:31:41 +0530

    Fixed Resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// This is a **project** GitHub Pages site:
-//   https://adityauke-systenics.github.io/panther-diamonds-demo-site/
-// so all asset URLs must include the repo name as base.
 export default defineConfig({
-  site: 'https://adityauke-systenics.github.io/panther-diamonds-demo-site',
-  base: '/panther-diamonds-demo-site/',
+  site: 'https://www.pantherdiamonds.com',
   integrations: [tailwind(), sitemap()],
-  output: 'static',
-  build: {
-    assets: '_assets',
-  },
 });
